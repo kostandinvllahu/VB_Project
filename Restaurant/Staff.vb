754ac0dd87b030f438a6a0fd324c1e4066891a77
kostandinvllahu <Ghostsechackers@gmail.com>
2021-04-26 13:30:04 +0200

    Fixing some bugs

--- Restaurant/Staff.vb
+++ Restaurant/Staff.vb
@@ -135,4 +135,33 @@ Public Class Staff
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         Clean()
     End Sub
+
+    Private Sub Guna2ImageButton1_Click(sender As Object, e As EventArgs) Handles Guna2ImageButton1.Click
+        Disp_data()
+        TextBox6.Text = ""
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If con.State = ConnectionState.Open Then
+            con.Close()
+        End If
+
+        con.Open()
+        Dim query As String = "SELECT  * FROM staff WHERE name='" + TextBox6.Text + "'"
+        Using con As SqlConnection = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\Documents\HotelVB.mdf;Integrated Security=True;Connect Timeout=30")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                Using da As SqlDataAdapter = New SqlDataAdapter()
+                    da.SelectCommand = cmd
+                    Using dt As New DataTable()
+                        da.Fill(dt)
+                        If dt.Rows.Count > 0 Then
+                            DataGridView1.DataSource = dt
+                        Else
+                            MessageBox.Show("No records found")
+                        End If
+                    End Using
+                End Using
+            End Using
+        End Using
+    End Sub
 End Class
