5ab406afdf19071a6ef34025b23268219361ec7e
kostandinvllahu <Ghostsechackers@gmail.com>
2021-06-01 18:27:22 +0200

    Combo box shows rooms that are active and non active are not shown

--- Restaurant/Book.vb
+++ Restaurant/Book.vb
@@ -10,6 +10,23 @@ Public Class Book
         End If
         con.Open()
         fillClientcombo()
+        fillRoomcombo()
+    End Sub
+
+    Public Sub fillRoomcombo()
+        Dim RoomBook As String = "Available"
+        Dim cmd As New SqlCommand("select id, roomtype from Room where roomstate='" + RoomBook + "'", con)
+        Dim da As New SqlDataAdapter
+        da.SelectCommand = cmd
+        Dim table As New DataTable
+        da.Fill(table)
+        Dim row As DataRow = table.NewRow
+        row(0) = 0
+        row(1) = "Select Room"
+        table.Rows.InsertAt(row, 0)
+        ComboBox2.DataSource = table
+        ComboBox2.DisplayMember = "roomtype"
+        ComboBox2.ValueMember = "id"
     End Sub
 
     Public Sub fillClientcombo()
@@ -32,11 +49,17 @@ Public Class Book
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
         TextBox1.Text = ComboBox1.SelectedIndex.ToString
 
-        Dim Query As String = "select * name from clients where id='" & TextBox1.Text & "'"
-        Dim Command As New Command(Query, con)
-        READER = Command.ExecuteReader
-        While READER.Read
-            TextBox1.Text = READER.GetString("name")
+        '  Dim Query As String = "select * name from clients where id='" & TextBox1.Text & "'"
+        '   Dim Command As New Command(Query, con)
+        '   READER = Command.ExecuteReader
+        '   While READER.Read
+        '  TextBox1.Text = READER.GetString("name")
 
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim menu As Menu = New Menu()
+        menu.Show()
+        Me.Close()
     End Sub
 End Class
